0e8c0148227c5f39e6f6553d134202b8f294beae
Jatin Kumar <jatink.net@gmail.com>
2021-12-17 19:08:10 +0530

    CRUD functionality implemented using Blazor server and EFCore

---
+++ blazor-server-crud/Data/BlazorDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blazor_server_crud.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazor_server_crud.Data
+{
+    public class BlazorDemoContext : DbContext
+    {
+        public BlazorDemoContext(DbContextOptions<BlazorDemoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Gadgets> Gadgets { get; set; }
+    }
+}
---
+++ blazor-server-crud/Data/Entities/Gadgets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_server_crud.Data.Entities
+{
+   
+        public class Gadgets
+        {
+            public int Id { get; set; }
+            public string ProductName { get; set; }
+            public string Brand { get; set; }
+            public decimal Cost { get; set; }
+            public string Type { get; set; }
+        }
+  
+}
